351d9317d22785b4f3f615282cdeb04ff622f12c
timok <timokaalma@gmail.com>
2023-10-23 21:25:31 +0300

    added a conection string

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<SchoolContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
