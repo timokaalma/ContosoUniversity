727c639e649d2f30cc6c7b23789daee08ad3e90f
timok <timokaalma@gmail.com>
2023-10-30 14:23:22 +0200

    Create StudentsController

--- Program.cs
+++ Program.cs
@@ -46,7 +46,7 @@ static IHostBuilder CreateHostBuilder(string[] args) =>
     Host.CreateDefaultBuilder(args)
         .ConfigureWebHostDefaults(webBuilder =>
         {
-            webBuilder.UseStartup<Startup>();
+            webBuilder.UseStartup<Program>();
         });
 
 
