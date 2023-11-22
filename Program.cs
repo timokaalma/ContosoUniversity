c8bb9d97c848bca0875129f5b39135d4604d88d0
timok <timokaalma@gmail.com>
2023-11-22 16:03:13 +0200

    Initializing db

--- Program.cs
+++ Program.cs
@@ -1,68 +1,84 @@
 using ContosoUniversity.Data;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<SchoolContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-
-var app = builder.Build();
-
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
-
-
- static void CreateDbIfNotExists(IHost host)
+namespace ContosoUniversity
 {
-    using (var scope = host.Services.CreateScope())
+    public class Program
     {
-        var services = scope.ServiceProvider;
-        try
+        public static void Main(string[] args)
         {
-            var context = services.GetRequiredService<SchoolContext>();
-            DbInitializer.Initialize(context);
-        }
-        catch (Exception ex)
-        {
-            var logger = services.GetRequiredService<ILogger<Program>>();
-            logger.LogError(ex, "An error occurred creating the DB.");
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add configuration.
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Configure services: register the SchoolContext as a service, specifying that it should use SQL Server as the database provider.
+            builder.Services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // Add the database exception filter if the environment is in development mode
+            if (builder.Environment.IsDevelopment())
+            {
+                builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            }
+
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //Building the Application
+            var app = builder.Build();
+
+            // Create a database and seed it with initial data
+            CreateDbIfNotExists(app);
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+
+
+            static void CreateDbIfNotExists(IHost host)
+            {
+                using (var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    try
+                    {
+                        var context = services.GetRequiredService<SchoolContext>();
+                        DbInitializer.Initialize(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        var logger = services.GetRequiredService<ILogger<Program>>();
+                        logger.LogError(ex, "An error occurred creating the DB.");
+                    }
+                }
+            }
         }
     }
 }
 
-static IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder =>
-        {
-            webBuilder.UseStartup<Program>();
-        });
 
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+
